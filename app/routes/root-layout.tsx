5957bf7e1af337d1bae5507cd547b50282f83fce
kanaha0610 <kanahakikannriko@fuji.waseda.jp>
2026-09-12 16:43:40 +0900

    fix:通知の表示

--- app/routes/root-layout.tsx
+++ app/routes/root-layout.tsx
@@ -5,6 +5,7 @@ import { BaseError } from "~/lib/error"
 import { repositoryMiddleware } from "~/middlewares/repositories"
 import { commitSession, getSession } from "~/sessions/sessions"
 import type { Route } from "./+types/root-layout"
+import { useEffect } from "react"
 
 export const middleware: Route.MiddlewareFunction[] = [repositoryMiddleware]
 
@@ -18,18 +19,19 @@ export async function loader({ request }: Route.LoaderArgs) {
 }
 
 export default function RootLayout({ loaderData }: Route.ComponentProps) {
+	useEffect(() => {
 	if (loaderData.toastPayload) {
-		// 副作用
 		showToast(loaderData.toastPayload)
 	}
+}, [loaderData.toastPayload])
 
 	return (
 		<>
-			<Toaster position="top-center" />
 			<Outlet />
+			<Toaster richColors position="top-center" />
 		</>
 	)
-}
+}	
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
 	if (isRouteErrorResponse(error)) {
